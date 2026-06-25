09a0b329acaa1093e85511e83d6da991628db0ee
Rashid Husain <rashid.husain@techademy.com>
2026-06-25 18:53:54 +0530

    worked on intial project

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+      <div className="main-container">
+        <div>
+          <p className="firstName">Rufayda Khatoon</p>
+          <p id="lastName">Shaista Khatoon</p>
+          <h1 className="lastName">Nahida Khatoon</h1>
+          <p>Count: {count}</p>
+          <div className="button-spacer">
+            <button className="button" onClick={() => setCount((count) => count + 1)}>
+              +
+            </button>
+            <button className="button" onClick={() => setCount((count) => count - 1)}>
+              -
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    
+    return (
+        <>
+            
+          Home, About, Contact, Careers, Services, Blog, FAQ, Support
+        </>
+    )
+}
+    
+export default Header;
